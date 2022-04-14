1b34a6f1ae30c46c36febee54973ef34bdc425cc
Digvijay Jaiswar <digvijay.jaiswar@inquizity.com>
2022-04-14 11:41:48 +0530

    Quick Sort Middle Pivot (Recursion)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = {15,9,7,13,12,16,4,18,11};
+        int length = arr.length;
+
+        Main mc = new Main();
+        mc.quickSortRecursion(arr,0,length-1);
+        mc.printArray(arr);
+    }
+
+    public void quickSortRecursion(int[] arr, int low, int high){
+        int pi = partition(arr, low, high);
+        if(low < pi-1){
+            quickSortRecursion(arr, low, pi-1);
+        }
+        if(high > pi){
+            quickSortRecursion(arr,pi,high);
+        }
+    }
+
+    public int partition(int[] arr, int low, int high){
+        int pivot = arr[(low+high)/2];
+
+        while(low <= high){
+            while(arr[low] < pivot){
+                low++;
+            }
+            while(arr[high] > pivot){
+                high--;
+            }
+            if(low <= high){
+                int temp = arr[low];
+                arr[low] = arr[high];
+                arr[high] = temp;
+
+                low++;
+                high--;
+            }
+        }
+        return low;
+    }
+
+    public void printArray(int[] arr){
+        for(int i : arr){
+            System.out.print(i+" ");
+        }
     }
 }
