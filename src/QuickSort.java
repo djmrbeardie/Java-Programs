13fcadab6ac9248675c236fad1e4913a7f3a11fb
Digvijay Jaiswar <digvijay.jaiswar@inquizity.com>
2022-04-14 11:48:49 +0530

    Quick Sort Middle Pivot (Recursion)

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,11 +1,12 @@
-public class Main {
+public class QuickSort {
     public static void main(String[] args) {
         int[] arr = {15,9,7,13,12,16,4,18,11};
         int length = arr.length;
 
-        Main mc = new Main();
+        QuickSort mc = new QuickSort();
         mc.quickSortRecursion(arr,0,length-1);
         mc.printArray(arr);
+//comment
     }
 
     public void quickSortRecursion(int[] arr, int low, int high){
